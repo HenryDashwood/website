081df8d8228b8b1c01be75ea0dbcc2138655fffa
HenryDashwood <hcndashwood@gmail.com>
2025-10-25 09:29:30 +0100

    minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
 import "katex/dist/katex.min.css";
 import localFont from "next/font/local";
